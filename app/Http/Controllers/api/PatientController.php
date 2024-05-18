2a5dc7d16a5284a800ce814bc6b279f5ba6383aa
Zarkprogame <zarkprogame@gmail.com>
2024-05-17 22:25:05 -0500

    Cracion del controlador para Pacientes"

--- app/Http/Controllers/api/PatientController.php
+++ app/Http/Controllers/api/PatientController.php
@@ -3,47 +3,61 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\PatientModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
-class PatientController extends Controller
+class PacienteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $pacientes = PatientModel::all();
+        return json_encode(['pacientes' => $pacientes]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $paciente = new PatientModel();
+        $paciente->nombre = $request->nombre;
+        $paciente->apellido = $request->apellido;
+        $paciente->fecha_nacimiento = $request->fecha_nacimiento;
+        $paciente->sexo = $request->sexo;
+        $paciente->telefono = $request->telefono;
+        $paciente->direccion = $request->direccion;
+        $paciente->email = $request->email;
+        $paciente->save();
+
+        return json_encode(['paciente' => $paciente]);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $paciente = PatientModel::find($id);
+
+        return json_encode(['paciente' => $paciente]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $paciente = PatientModel::find($id);
+
+        $paciente->nombre = $request->nombre;
+        $paciente->apellido = $request->apellido;
+        $paciente->fecha_nacimiento = $request->fecha_nacimiento;
+        $paciente->sexo = $request->sexo;
+        $paciente->telefono = $request->telefono;
+        $paciente->direccion = $request->direccion;
+        $paciente->email = $request->email;
+        $paciente->save();
+
+        return json_encode(['paciente' => $paciente]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $paciente = PatientModel::find($id);
+        $paciente->delete();
+
+        return json_encode(['success' => true]);
     }
 }

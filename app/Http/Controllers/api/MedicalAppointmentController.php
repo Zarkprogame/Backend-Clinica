435d285ed8101e5b74abc7c2b8b3499e4548d7e9
Zarkprogame <zarkprogame@gmail.com>
2024-05-19 17:26:54 -0500

    Controlador de citas medicas terminado, y configuracion de rutas

--- app/Http/Controllers/api/MedicalAppointmentController.php
+++ app/Http/Controllers/api/MedicalAppointmentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\MedicalAppointmentModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MedicalAppointmentController extends Controller
 {
@@ -12,7 +14,11 @@ class MedicalAppointmentController extends Controller
      */
     public function index()
     {
-        //
+        $appointments = DB::table('medical_appointments')
+            ->join('patients', 'medical_appointments.paciente_id', '=', 'patients.id')
+            ->select('medical_appointments.*', 'patients.nombre')
+            ->get();
+        return json_encode(['appointments' => $appointments]);
     }
 
     /**
@@ -20,7 +26,14 @@ class MedicalAppointmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $appointment = new MedicalAppointmentModel();
+        $appointment->id = $request->id;
+        $appointment->paciente_id = $request->paciente_id;
+        $appointment->fecha_hora = $request->fecha_hora;
+        $appointment->tipo = $request->tipo;
+        $appointment->estado = $request->estado;
+        $appointment->save();
+        return json_encode(['$appointment' => $appointment]);
     }
 
     /**
@@ -28,7 +41,11 @@ class MedicalAppointmentController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $appointment = MedicalAppointmentModel::find($id);
+        $patients = DB::table('patients')
+            ->orderBy('nombre')
+            ->get();
+        return json_encode(['appointment' => $appointment, 'patients' => $patients]);
     }
 
     /**
@@ -36,7 +53,15 @@ class MedicalAppointmentController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $appointment = MedicalAppointmentModel::find($id);
+        $appointment->id = $request->id;
+        $appointment->paciente_id = $request->paciente_id;
+        $appointment->fecha_hora = $request->fecha_hora;
+        $appointment->tipo = $request->tipo;
+        $appointment->estado = $request->estado;
+        $appointment->save();
+
+        return json_encode(['$appointment' => $appointment]);
     }
 
     /**
@@ -44,6 +69,14 @@ class MedicalAppointmentController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $appointment = MedicalAppointmentModel::find($id);
+        $appointment->destroy();
+
+        $appointments = DB::table('medical_appointments')
+            ->join('patients', 'medical_appointments.paciente_id', '=', 'patients.id')
+            ->select('medical_appointments.*', 'patients.nombre')
+            ->get();
+
+        return json_encode(['appointments' => $appointments, 'success' => true]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\MedicalRecordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\PatientController;
@@ -13,3 +14,9 @@ Route::get('/pacientes', [PatientController::class, 'index'])->name('pacientes')
 Route::delete('/pacientes/{paciente}', [PatientController::class, 'destroy'])->name('pacientes.destroy');
 Route::get('/pacientes/{paciente}', [PatientController::class, 'show'])->name('pacientes.show');
 Route::put('/pacientes/{paciente}', [PatientController::class, 'update'])->name('pacientes.update');
+
+Route::post('/appointments', [MedicalRecordController::class, 'store'])->name('appointments.store');
+Route::get('/appointments', [MedicalRecordController::class, 'index'])->name('appointments');
+Route::delete('/appointments/{appointment}', [MedicalRecordController::class, 'destroy'])->name('appointments.destroy');
+Route::get('/appointments/{appointment}', [MedicalRecordController::class, 'show'])->name('appointments.show');
+Route::put('/appointments/{appointment}', [MedicalRecordController::class, 'update'])->name('appointments.update');

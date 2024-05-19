2de81da0685183775421665f9eb4053742a8b15b
Zarkrprogame <zarkrprogame@gmail.com>
2024-05-19 18:19:04 -0500

    Correcion en las rutas para Citas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\api\MedicalRecordController;
+use App\Http\Controllers\api\MedicalAppointmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\PatientController;
@@ -15,8 +15,8 @@ Route::delete('/pacientes/{paciente}', [PatientController::class, 'destroy'])->n
 Route::get('/pacientes/{paciente}', [PatientController::class, 'show'])->name('pacientes.show');
 Route::put('/pacientes/{paciente}', [PatientController::class, 'update'])->name('pacientes.update');
 
-Route::post('/appointments', [MedicalRecordController::class, 'store'])->name('appointments.store');
-Route::get('/appointments', [MedicalRecordController::class, 'index'])->name('appointments');
-Route::delete('/appointments/{appointment}', [MedicalRecordController::class, 'destroy'])->name('appointments.destroy');
-Route::get('/appointments/{appointment}', [MedicalRecordController::class, 'show'])->name('appointments.show');
-Route::put('/appointments/{appointment}', [MedicalRecordController::class, 'update'])->name('appointments.update');
+Route::post('/appointments', [MedicalAppointmentController::class, 'store'])->name('appointments.store');
+Route::get('/appointments', [MedicalAppointmentController::class, 'index'])->name('appointments');
+Route::delete('/appointments/{appointment}', [MedicalAppointmentController::class, 'destroy'])->name('appointments.destroy');
+Route::get('/appointments/{appointment}', [MedicalAppointmentController::class, 'show'])->name('appointments.show');
+Route::put('/appointments/{appointment}', [MedicalAppointmentController::class, 'update'])->name('appointments.update');
